baf197b822b144410716728a7260d9d27ff4a0e1
PualaniL <130610369+PualaniL@users.noreply.github.com>
2023-06-28 17:31:49 -0700

    made some quick changes

---
+++ Week 1/script.js
@@ -0,0 +1,6 @@
+var firstName = "John";
+var lastName = "Doe";
+var yearsOfStudy = 3;
+var goal = "pursue a career in programming";
+
+document.write("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
